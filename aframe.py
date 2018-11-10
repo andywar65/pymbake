f6c02c06e54d9b5f5fd313a2e7af6c7c7d306f5d
andywar65 <andy.war1965@gmail.com>
2018-11-10 01:36:26 +0100

    leg is an attribute of furniture class

--- aframe.py
+++ aframe.py
@@ -1416,30 +1416,30 @@ class AFurniture(object):
         outstr += f'material="src: #image-{self.d["tiling_image"]}; color: {self.d["tiling_color"]} '
         outstr += is_repeat(self.d["tiling_repeat"], fabs(self.d["41"]), fabs(self.d["42"]))
         outstr += '"></a-box>\n'
-        leg = self.d["43"]-0.025*unit(self.d["43"])
+        self.leg = self.d["43"]-0.025*unit(self.d["43"])
         #first leg
         outstr += f'<a-cylinder id="{self.d["2"]}-{self.d["num"]}-leg-1" \n'
         outstr += f'position="{0.05*unit(self.d["41"])} {leg/2} {-0.05*unit(self.d["42"])}" \n'
-        outstr += self.close_leg(leg)
+        outstr += self.close_leg()
         #second leg
         outstr += f'<a-cylinder id="{self.d["2"]}-{self.d["num"]}-leg-2" \n'
         outstr += f'position="{self.d["41"]-0.05*unit(self.d["41"])} {leg/2} {-0.05*unit(self.d["42"])}" \n'
-        outstr += self.close_leg(leg)
+        outstr += self.close_leg()
         #third leg
         outstr += f'<a-cylinder id="{self.d["2"]}-{self.d["num"]}-leg-3" \n'
         outstr += f'position="{0.05*unit(self.d["41"])} {leg/2} {-self.d["42"]+0.05*unit(self.d["42"])}" \n'
-        outstr += self.close_leg(leg)
+        outstr += self.close_leg()
         #fourth leg
         outstr += f'<a-cylinder id="{self.d["2"]}-{self.d["num"]}-leg-4" \n'
         outstr += f'position="{self.d["41"]-0.05*unit(self.d["41"])} {leg/2} {-self.d["42"]+0.05*unit(self.d["42"])}" \n'
-        outstr += self.close_leg(leg)
+        outstr += self.close_leg()
         #close entity
         outstr += '</a-entity>\n'
         return outstr
 
-    def close_leg(self, leg):
+    def close_leg(self):
         outstr = 'radius="0.025" \n'
-        outstr += f'height="{leg}" \n'
+        outstr += f'height="{self.leg}" \n'
         outstr += f'material="src: #image-{self.d["skirting_image"]}; color: {self.d["skirting_color"]} '
         outstr += '"></a-cylinder>\n'
         return outstr
